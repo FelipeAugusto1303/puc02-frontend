c6200de6267ad1a810b2aa459f15f35cb2c2d9a3
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-12-06 20:44:41 -0400

    feat: create project, fake database and configuring route

---
+++ src/database/database.js
@@ -0,0 +1,27 @@
+export const usersList = [
+  {
+    id: 1,
+    username: "admin@email.com",
+    password: "admin",
+    fullName: "Administrator",
+    address: "Neverland",
+  },
+];
+
+export const doctorsList = [
+  {
+    id: 1,
+    name: "Rodrigo Pessoa",
+    area: "Ortopedista",
+    address: "Neverland Avenue, 94",
+  },
+];
+
+export const schedulingList = [
+  {
+    id: 1,
+    userId: 1,
+    doctorId: 1,
+    schedule_date: "2023-12-06 15:26:30",
+  },
+];
